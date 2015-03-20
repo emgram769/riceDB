a1d0a38c35205c8dadd45de90abc7973ac279575
bramserv <shared@bwasti.com>
2015-03-21 01:22:09 +0300

    fixed server

--- server/server.py
+++ server/server.py
@@ -1,9 +1,11 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler, HTTPServer, SimpleHTTPRequestHandler
 import time
 import random
+import threading
 
-hostName = "localhost"
+hostName = "0.0.0.0"
 hostPort = 9000
+staticPort = 9001
 
 class MyServer(BaseHTTPRequestHandler):
   def do_GET(self):
@@ -16,33 +18,42 @@ class MyServer(BaseHTTPRequestHandler):
   "Name": "%s repo",
   "Program": "i3",
   "Description": "This is a sample of the description field.",
-  "URL": "http://localhost:8000/test.zip",
-  "Images": ["http://localhost:8000/test.jpg"]
+  "URL": "http://bwasti.com:9001/test.zip",
+  "Images": ["http://bwasti.com:9001/test.jpg"]
 },
 {
   "Name": "%s repo 2",
   "Program": "i3",
   "Description": "Does this description thing work?.",
-  "URL": "http://localhost:8000/test.zip",
-  "Images": ["http://localhost:8000/test2.jpg"]
+  "URL": "http://bwasti.com:9001/test.zip",
+  "Images": ["http://bwasti.com:9001/test2.jpg"]
 },
 {
   "Name": "%s repo 3",
   "Program": "i3",
   "Description": "test.",
-  "URL": "http://localhost:8000/test.zip",
-  "Images": ["http://localhost:8000/test3.png"]
+  "URL": "http://bwasti.com:9001/test.zip",
+  "Images": ["http://bwasti.com:9001/test3.png"]
 }
 ]""" % (self.path[1:], self.path[1:], self.path[1:])
     self.wfile.write(bytes(out, "utf-8"))
 
-myServer = HTTPServer((hostName, hostPort), MyServer)
+queryServer = HTTPServer((hostName, hostPort), MyServer)
 print(time.asctime(), "Server Starts - %s:%s" % (hostName, hostPort))
 
+def staticServer(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
+    server_address = (hostName, staticPort)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+s = threading.Thread(target=staticServer)
+s.setDaemon(True)
+s.start()
+
 try:
-  myServer.serve_forever()
+  queryServer.serve_forever()
 except KeyboardInterrupt:
   pass
 
-myServer.server_close()
+queryServer.server_close()
 print(time.asctime(), "Server Stops - %s:%s" % (hostName, hostPort))
