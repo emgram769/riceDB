c61a03640be2b2c3db2ed0bc749abbea5cacf587
Bram Wasti <bwasti@gmail.com>
2015-03-20 16:46:54 -0400

    added default server

--- client/rice/query.py
+++ client/rice/query.py
@@ -5,17 +5,23 @@
 
 import urllib.request
 import json
+import os
 from rice import error, util, package
 
 class Query(object):
     def __init__(self, query):
-      with open(util.RDBDIR + "config") as config_file:
-        try:
-          config = json.load(config_file)
-        except Exception as e:
-          raise error.corruption_error("Invalid JSON: %s" %(e))
+      config_path = util.RDBDIR + "config"
+      if os.path.exists(config_path) and os.path.isfile(config_path):
+        with open(util.RDBDIR + "config") as config_file:
+          try:
+            config = json.load(config_file)
+          except Exception as e:
+            raise error.corruption_error("Invalid JSON: %s" %(e))
+      else:
+        config = dict()
+        config["dbs"] = ["http://bwasti.com:9000/"]
       try:
-        request = urllib.request.Request(config["db"] + query)
+        request = urllib.request.Request(config["dbs"][0] + query)
         response = urllib.request.urlopen(request).read().decode('utf-8')
       except Exception as e:
         raise error.Error("Could not connect to server %s: %s" % (config["db"], e))
